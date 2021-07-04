e3582e1d4a93c9ade5c94a27503a3e2f16a2a10d
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-07-04 09:06:37 +0900

    add TripButton

---
+++ src/components/atoms/TripButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    :class="classes"
+    :disabled="disabled"
+    @click.prevent="handleClick">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'TripButton',
+  props: {
+    label: {
+      type: String,
+      default: 'Button'
+    },
+    disabled: {
+      type: Boolean,
+      defalt: false
+    }
+  },
+  computed: {
+    classes(){
+      if (this.disabled){
+        return 'not-allowed'
+      }
+      return ''
+    }
+  },
+  methods: {
+    handleClick(ev){
+      this.$emit('click', ev)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.not-allowed{
+  cursor: not-allowed;
+}
+
+</style>
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,39 +1,27 @@
-import MyButton from './Button.vue'
-
+import TripButton from '../components/atoms/TripButton'
 export default {
-  title: 'Example/Button',
-  component: MyButton,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
-  },
+  title: 'Atoms/Button',
+  component: TripButton,
 }
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { MyButton },
-  template: '<my-button @onClick="onClick" v-bind="$props" />',
+  components: { TripButton },
+  template: `
+  <div>
+    <trip-button @click="onClick" v-bind="$props" />
+  </div>
+  `,
 })
 
-export const Primary = Template.bind({})
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Normal = Template.bind({})
+Normal.args = {
+  label: 'normal',
+  disabled: true
 }
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-  label: 'Button',
-}
-
-export const Large = Template.bind({})
-Large.args = {
-  size: 'large',
-  label: 'Button',
-}
-
-export const Small = Template.bind({})
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Disabled = Template.bind({})
+Disabled.args = {
+  label: 'disabled',
+  disabled: true
 }

a677afdb86ade5fe8257d8d92fa3cf114922faee
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-07-12 23:39:49 +0900

    add TripHotelForm,TripPagination

---
+++ src/components/moleclues/TripHotelForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>キーワード検索</p>
+    <form @submit.prevent="search">
+      <input
+        v-model="keyword"
+        type="text">
+      <p>
+        <button type="submit">
+          検索
+        </button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TripHotelForm',
+  data(){
+    return {
+      keyword: ''
+    }
+  },
+  methods: {
+    search(){
+      this.$emit('search')
+      this.onClick()
+    },
+    onClick(){
+      this.$emit('onClick', this.keyword)
+    },
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/moleclues/TripPagination.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <TripIcon
+      class="trip-icon"
+      :disabled="disablePrev"
+      @onClick="prev">
+      &lt;前へ
+    </TripIcon>
+    {{ pageNum }}
+    <TripIcon
+      class="trip-icon"
+      :disabled="disableNext"
+      @onClick="next">
+      次へ&gt;
+    </TripIcon>
+  </div>
+</template>
+
+<script>
+import TripIcon from '../atoms/TripIcon.vue'
+export default {
+  name: 'TripPagination',
+  components: {
+    TripIcon
+  },
+  props: {
+    pageNum: {
+      type: Number,
+      require: true,
+      default: 0
+    },
+    disablePrev: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    disableNext: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  methods: {
+    prev() {
+      this.$emit('prev')
+    },
+    next(){
+      this.$emit('next')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.trip-icon{
+  display: inline-block;
+}
+</style>

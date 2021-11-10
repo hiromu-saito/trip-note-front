b08c20c4d2d87d91dd2d47cc52f5a9a449febb62
hiromu.saito <hiromu.saito@bigtreetc.com>
2021-11-10 22:51:56 +0900

    APIのパラメータ名修正

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import Auth from './auth'
 import Memory from './memory'
 
-const HOTEL_SEARCH_URL = `https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?applicationId=${process.env.VUE_APP_SECRET}&format=json&keyword=%keyword&hits=6&currentPage=%currentPage`
+const HOTEL_SEARCH_URL = `https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?applicationId=${process.env.VUE_APP_SECRET}&format=json&keyword=%keyword&hits=6&page=%page`
 export {
   Auth,
   Memory,
